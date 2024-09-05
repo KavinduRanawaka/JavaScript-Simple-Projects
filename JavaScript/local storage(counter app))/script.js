0eac7ae2151e0aad7d0ad876677e573d59bba1cc
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-05 11:45:44 +0530

    Update script.js

---
+++ JavaScript/local storage(counter app))/script.js
@@ -0,0 +1,8 @@
+function add(){
+   let cCount = parseInt(document.getElementById("count").innerHTML);
+   document.getElementById("count").innerHTML=cCount+1
+}
+function sub(){
+   let cCount = parseInt(document.getElementById("count").innerHTML);
+   document.getElementById("count").innerHTML=cCount-1
+}
