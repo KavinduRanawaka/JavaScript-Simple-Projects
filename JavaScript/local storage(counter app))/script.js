9a508a79085f5e7467cac91c55b1117c166b3681
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-05 12:04:24 +0530

    Update script.js

--- JavaScript/local storage(counter app))/script.js
+++ JavaScript/local storage(counter app))/script.js
@@ -1,8 +1,17 @@
+if(localStorage.getItem('count')==null){
+    document.getElementById('count').innerHTML="0";
+}
+else{
+    document.getElementById('count').innerHTML=localStorage.getItem('count');
+}
+
 function add(){
-   let cCount = parseInt(document.getElementById("count").innerHTML);
-   document.getElementById("count").innerHTML=cCount+1
-}
-function sub(){
-   let cCount = parseInt(document.getElementById("count").innerHTML);
-   document.getElementById("count").innerHTML=cCount-1
-}
+    let cCount = parseInt(document.getElementById("count").innerHTML);
+    document.getElementById("count").innerHTML=cCount+1;
+    localStorage.setItem('count',cCount+1)
+ }
+ function sub(){
+    let cCount = parseInt(document.getElementById("count").innerHTML);
+    document.getElementById("count").innerHTML=cCount-1;
+    localStorage.setItem('count',cCount-1)
+ }
